f063d66a6d73fbbc2431b86c71651b1d0671d3f5
GitHalex <calex1731@gmail.com>
2024-03-04 20:38:41 -0400

    agregando los eventos y archivos del teclado

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,14 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
-import { digitalClock } from "./dom/reloj.js";
-
+import { alarm, digitalClock } from "./dom/reloj.js";
+import { shortcuts } from "./dom/teclado.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("../assets/alarma_5.mp3", "#activar-alarma", "#desactivar-alarma");
+});
+
+d.addEventListener("keydown", (e) => {
+  shortcuts(e);
 });
