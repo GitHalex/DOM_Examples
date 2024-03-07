fb4b861fe2f278422409d9aa2376622ec8d5ce81
GitHalex <calex1731@gmail.com>
2024-03-06 21:03:06 -0400

    cuenta regresiva terminado

---
+++ js/dom/cuenta_regresiva.js
@@ -0,0 +1,33 @@
+const d = document;
+export default function countdown(id, limitDate, finalMessage) {
+  const $countdown = d.getElementById(id);
+  const $countdownDate = new Date(limitDate).getTime();
+
+  let countdownTempo = setInterval(() => {
+    let now = new Date().getTime();
+    console.log(now);
+    let limitTime = $countdownDate - now;
+    console.log(limitTime);
+    let days = Math.floor(limitTime / (1000 * 60 * 60 * 24));
+    console.log(days);
+    let hours = (
+      "0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    ).slice(-2);
+    console.log(hours);
+    let minutes = (
+      "0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))
+    ).slice(-2);
+    console.log(minutes);
+    let seconds = ("0" + Math.floor((limitTime % (1000 * 60)) / 1000)).slice(
+      -2
+    );
+    console.log(seconds);
+
+    $countdown.innerHTML = `<h2>Faltan: ${days} dias - ${hours} horas - ${minutes} minutos - ${seconds} segundos</h2>`;
+
+    if (limitTime < 0) {
+      clearInterval(countdownTempo);
+      $countdown.innerHTML = `<h3>${finalMessage}</h3>`;
+    }
+  }, 1000);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,12 +1,19 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { alarm, digitalClock } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("../assets/alarma_5.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown(
+    "countdown",
+    "May 23, 2024 03:23:19",
+    "Feliz Cumpleaños amigo y docente digital"
+  );
 });
 
 d.addEventListener("keydown", (e) => {
